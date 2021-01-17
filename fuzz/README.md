16e975fc38bc6f4b970ad980d52a0d01db731f9e
Sergio Benitez <sb@sergio.bz>
2021-01-16 17:27:44 -0800

    Add fuzzing targets.

---
+++ fuzz/README.md
@@ -0,0 +1,15 @@
+# Fuzzing
+
+Install `cargo-fuzz`:
+
+```sh
+cargo install -f cargo-fuzz
+```
+
+Run any available target where `$target` is the name of the target and `$n` is
+the number of CPUs to use for fuzzing:
+
+```sh
+cargo fuzz list # get list of targets
+cargo fuzz $target -j $n
+```
---
+++ fuzz/fuzz_targets/fuzz_multipart_bytes.rs
@@ -0,0 +1,28 @@
+#![no_main]
+
+use std::convert::Infallible;
+
+use multer::Multipart;
+use multer::bytes::Bytes;
+use futures::stream::once;
+use libfuzzer_sys::fuzz_target;
+use tokio::runtime;
+
+fuzz_target!(|data: &[u8]| {
+    let data = data.to_vec();
+    let stream = once(async move { Result::<Bytes, Infallible>::Ok(Bytes::from(data)) });
+
+    let mut multipart = Multipart::new(stream, "X-BOUNDARY");
+
+    let rt = runtime::Builder::new_current_thread().build().expect("runtime");
+    rt.block_on(async {
+        let mut breaks = 0;
+        while breaks < 3 {
+            let field = multipart.next_field().await;
+            match field {
+                Err(_) | Ok(None) => breaks += 1,
+                Ok(Some(_)) => continue,
+            }
+        }
+    })
+});
