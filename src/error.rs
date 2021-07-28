493d39bd1df5158b8ed406f01779e5d4133de0d4
Sergio Benitez <sb@sergio.bz>
2021-07-28 15:12:59 -0700

    Remove 'derive_more' dependency.
    This reduces the number of transitive dependencies to 37 and decreases
    the clean build-time three-fold (33% of the previous).

--- src/error.rs
+++ src/error.rs
@@ -1,91 +1,104 @@
 use std::fmt::{self, Debug, Display, Formatter};
 
-use derive_more::Display;
-
 type BoxError = Box<dyn std::error::Error + Send + Sync>;
 
 /// A set of errors that can occur during parsing multipart stream and in other
 /// operations.
-#[derive(Display)]
 #[non_exhaustive]
 pub enum Error {
     /// An unknown field is detected when multipart
     /// [`constraints`](crate::Constraints::allowed_fields) are added.
-    #[display(fmt = "unknown field received: {}", "field_name.as_deref().unwrap_or(\"<unknown>\")")]
     UnknownField { field_name: Option<String> },
 
     /// The field data is found incomplete.
-    #[display(
-        fmt = "field '{}' received with incomplete data",
-        "field_name.as_deref().unwrap_or(\"<unknown>\")"
-    )]
     IncompleteFieldData { field_name: Option<String> },
 
     /// Couldn't read the field headers completely.
-    #[display(fmt = "failed to read field complete headers")]
     IncompleteHeaders,
 
     /// Failed to read headers.
-    #[display(fmt = "failed to read headers: {}", _0)]
     ReadHeaderFailed(httparse::Error),
 
     /// Failed to decode the field's raw header name to
     /// [`HeaderName`](http::header::HeaderName) type.
-    #[display(fmt = "failed to decode field's raw header name: {:?} {}", name, cause)]
     DecodeHeaderName { name: String, cause: BoxError },
 
     /// Failed to decode the field's raw header value to
     /// [`HeaderValue`](http::header::HeaderValue) type.
-    #[display(fmt = "failed to decode field's raw header value: {}", cause)]
     DecodeHeaderValue { value: Vec<u8>, cause: BoxError },
 
     /// Multipart stream is incomplete.
-    #[display(fmt = "incomplete multipart stream")]
     IncompleteStream,
 
     /// The incoming field size exceeded the maximum limit.
-    #[display(
-        fmt = "field '{}' exceeded the maximum size limit: {} bytes",
-        "field_name.as_deref().unwrap_or(\"<unknown>\")",
-        limit
-    )]
     FieldSizeExceeded { limit: u64, field_name: Option<String> },
 
     /// The incoming stream size exceeded the maximum limit.
-    #[display(fmt = "stream size exceeded the maximum limit: {} bytes", limit)]
     StreamSizeExceeded { limit: u64 },
 
     /// Stream read failed.
-    #[display(fmt = "stream read failed: {}", _0)]
     StreamReadFailed(BoxError),
 
     /// Failed to lock the multipart shared state for any changes.
-    #[display(fmt = "failed to lock multipart state")]
     LockFailure,
 
     /// The `Content-Type` header is not `multipart/form-data`.
-    #[display(fmt = "Content-Type is not multipart/form-data")]
     NoMultipart,
 
     /// Failed to convert the `Content-Type` to [`mime::Mime`] type.
-    #[display(fmt = "Failed to convert Content-Type to `mime::Mime` type: {}", _0)]
     DecodeContentType(mime::FromStrError),
 
     /// No boundary found in `Content-Type` header.
-    #[display(fmt = "multipart boundary not found in Content-Type")]
     NoBoundary,
 
     /// Failed to decode the field data as `JSON` in
     /// [`field.json()`](crate::Field::json) method.
     #[cfg(feature = "json")]
     #[cfg_attr(nightly, doc(cfg(feature = "json")))]
-    #[display(fmt = "failed to decode field data as JSON: {}", _0)]
     DecodeJson(serde_json::Error),
 }
 
 impl Debug for Error {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         Display::fmt(self, f)
+    }
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            Error::UnknownField { field_name } => {
+                let name = field_name.as_deref().unwrap_or("<unknown>");
+                write!(f, "unknown field received: {:?}", name)
+            }
+            Error::IncompleteFieldData { field_name } => {
+                let name = field_name.as_deref().unwrap_or("<unknown>");
+                write!(f, "field {:?} received with incomplete data", name)
+            }
+            Error::DecodeHeaderName { name, cause } => {
+                write!(f, "failed to decode field's raw header name: {:?} {}", name, cause)
+            }
+            Error::DecodeHeaderValue { cause, .. } => {
+                write!(f, "failed to decode field's raw header value: {}", cause)
+            }
+            Error::FieldSizeExceeded { limit, field_name } => {
+                let name = field_name.as_deref().unwrap_or("<unknown>");
+                write!(f, "field {:?} exceeded the size limit: {} bytes", name, limit)
+            }
+            Error::StreamSizeExceeded { limit } => {
+                write!(f, "stream size exceeded limit: {} bytes", limit)
+            }
+            Error::ReadHeaderFailed(e) => write!(f, "failed to read headers: {}", e),
+            Error::StreamReadFailed(e) => write!(f, "failed to read stream: {}", e),
+            Error::DecodeContentType(e) => write!(f, "failed to decode Content-Type: {}", e),
+            Error::IncompleteHeaders => write!(f, "failed to read field complete headers"),
+            Error::IncompleteStream => write!(f, "incomplete multipart stream"),
+            Error::LockFailure => write!(f, "failed to lock multipart state"),
+            Error::NoMultipart => write!(f, "Content-Type is not multipart/form-data"),
+            Error::NoBoundary => write!(f, "multipart boundary not found in Content-Type"),
+            #[cfg(feature = "json")]
+            Error::DecodeJson(e) => write!(f, "failed to decode field data as JSON: {}", e),
+        }
     }
 }
 
